3de67d9553d3125b7a2758d876702c8c1cbfbbab
Anurag Pramod Shingare <anuragshingare8@gmail.com>
2024-11-08 21:40:38 +0530

    test

--- test/Web3.t.sol
+++ test/Web3.t.sol
@@ -1,18 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 import "../src/Web3.sol";
 
 contract Web3Test is Test{
 
     TestContract token;
 
-    function setup() public {
+    function setUp() public {
         token = new TestContract();
     }
 
     function testName() public view{
+        console.log(token.name());
         assertEq(token.name(), "test");
+    }
+
+    function testGetName() public{ 
+        token.setName("anurag");
+        assertEq(token.getName(), "anurag");
     }
 }
