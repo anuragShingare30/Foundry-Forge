11e3ceaf6abc2fc72e0c27b724c0de230bd5818f
Anurag Pramod Shingare <anuragshingare8@gmail.com>
2024-11-09 23:04:37 +0530

    test

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -7,8 +7,8 @@ import {TestContract} from "../src/Web3.sol";
 contract MyScript is Script{
     
     function run() external returns(TestContract){
-        // This loads in the private key from our .env file
-        uint256 privateKey = vm.envUint("ANVIL_PRIVATE_KEY");
+        // // This loads in the private key from our .env file
+        uint256 privateKey = vm.envUint("PRIVATE_KEY");
 
         // a special cheatcode that records calls and contract creations made by our main script contract.
         vm.startBroadcast(privateKey);
@@ -26,7 +26,17 @@ contract MyScript is Script{
 
 // // DEPLOYING LOCALLY(ANVIL)
 // forge script script/Deploy.s.sol:MyScript --fork-url http://localhost:8545 --broadcast
+// forge script script/Deploy.s.sol:MyScript --fork-url http://localhost:8545 --account <account_name> --sender <address> --broadcast
 
 
 // // DEPLOYING AND VERIFYING ON TESTNET
 // forge script --chain sepolia script/Deploy.s.sol:MyScript --rpc-url $SEPOLIA_RPC_URL --broadcast --verify -vvvv
+// forge script --chain sepolia script/Deploy.s.sol:MyScript --rpc-url $SEPOLIA_RPC_URL --account <account_name> --sender <address> --broadcast --verify -vvvv
+
+// storing private key
+// cast wallet list
+// cast wallet import privateKey --interactive
+
+// interacting
+// cast send <address> "setName(string)" anurag --rpc-url <rpc_url> --private-key <private_key>
+// cast call <address> "getName()"
