c81bad01ccc675c963a2f3d8c9ec8edc28ed14b7
Anurag Pramod Shingare <anuragshingare8@gmail.com>
2024-11-11 07:22:13 +0530

    test

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -8,7 +8,7 @@ contract MyScript is Script{
     
     function run() external returns(TestContract){
         // // This loads in the private key from our .env file
-        uint256 privateKey = vm.envUint("PRIVATE_KEY");
+        uint256 privateKey = vm.envUint("ANVIL_PRIVATE_KEY");
 
         // a special cheatcode that records calls and contract creations made by our main script contract.
         vm.startBroadcast(privateKey);
@@ -21,25 +21,3 @@ contract MyScript is Script{
         return token;
     }
 }
-
-
-
-// // DEPLOYING LOCALLY(ANVIL)
-// forge script script/Deploy.s.sol:MyScript --fork-url http://localhost:8545 --broadcast
-// forge script script/Deploy.s.sol:MyScript --fork-url http://localhost:8545 --account <account_name> --sender <address> --broadcast
-
-
-// // DEPLOYING AND VERIFYING ON TESTNET
-// forge script script/Deploy.s.sol:MyScript --rpc-url $SEPOLIA_RPC_URL --broadcast --verify -vvvv
-// forge script script/Deploy.s.sol:MyScript --rpc-url $SEPOLIA_RPC_URL --account <account_name> --sender <address> --broadcast --verify -vvvv
-// forge script script/Deploy.s.sol:MyScript --rpc-url $SEPOLIA_RPC_URL --private-key $PRIVATE_KEY --broadcast --verify -vvvv
-
-
-// storing private key
-// cast wallet list
-// cast wallet import privateKey --interactive
-
-// interacting
-// cast send <address> "setName(string)" anurag --rpc-url <rpc_url> --private-key <private_key>
-// cast call <address> "getName()"
-// cast to-base 0x7717 dec
--- test/Web3.t.sol
+++ test/Web3.t.sol
@@ -3,27 +3,52 @@ pragma solidity ^0.8.0;
 
 import {Test, console} from "forge-std/Test.sol";
 import {TestContract} from "../src/Web3.sol";
+import {MyScript} from "../script/Deploy.s.sol";
 
 contract Web3Test is Test{
 
     TestContract token;
 
     function setUp() external {
+        // BY THIS WAY WE NEED TO JUST UPDATE THE DEPLOY SCRIPT
         // NEW SC INSTANCE
-        token = new TestContract();
+        MyScript myscript = new MyScript();
+        token = myscript.run();
     }
 
-    function testSetName() public view{
-        console.log(token.name());
-        assertEq(token.name(), "test");
+    function testBalance() public view {
+        console.log(token.publicMintOpen());
+        assertEq(token.publicMintOpen(), false);
     }
 
-    function testGetName() public{ 
-        token.setName("anurag");
-        assertEq(token.getName(), "anurag");
+    function testMint() public{
+        vm.expectRevert();
+        token.publicMint();
     }
+
+    
 }
 
 
+// 1. UNIT TESTING - TESTING A SPECIFIC PART OF OUR CODE.
+// 2. INTEGRATION TEST - INTEGRATING SC A TESTING SPECIFIC PORTION.
+// 3. FORKED TEST - TESTING OUR CODE ON A SIMULATED REAL ENVIRONMENT.
+// 4. STAGING TEST - TESTING OUR CODE IN TESTNET/MAINNET
+
+
+
+// TESTING SC
 // forge test -vvv
+// forge test --fork-url $RPC_URL
+
+// TO RUN THE SINGLE TEST
+// forge test -m testBalance -vvv --fork-url $RPC_URL
+
+// CONVERGING SC - This command displays which parts of your code are covered by tests.
+// forge converge --fork-url $RPC_URL
+
+// DEBUGGING SC
 // forge debug --debug src/Web3.sol:TestContract --sig "setName()" "anurag"
+
+// Forge can remap dependencies to make them easier to import. Forge will automatically try to deduce some remappings for you:
+// forge remappings
