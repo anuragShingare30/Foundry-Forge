9f39cbf77d602e38e04a481742d0f6f916db592d
Anurag Pramod Shingare <anuragshingare8@gmail.com>
2024-12-05 23:19:45 +0530

    test commit

--- test/unit/Web3.t.sol
+++ test/unit/Web3.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {Test, console} from "../../lib/foundry-devops/test/DevOpsToolsTest.t.sol";
+import {Test} from "../../lib/foundry-devops/test/DevOpsToolsTest.t.sol";
 import {TestContract} from "../../src/Web3.sol";
 import {MyScript} from "../../script/Deploy.s.sol";
 
@@ -17,7 +17,7 @@ contract Web3Test is Test{
     }
 
     function testBalance() public view {
-        console.log(token.publicMintOpen());
+        // console.log(token.publicMintOpen());
         assertEq(token.publicMintOpen(), false);
     }
 
